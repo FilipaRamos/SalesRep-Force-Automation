9b738fa3782906590b9f2d4e5fea0c6ac649985a
Gil Domingues <gil.domingues2@hotmail.com>
2016-11-16 14:17:15 +0000

    Started backend
    Started sales force automation, switching to backend branch

---
+++ sfa-backend/SalesForceAutomation/SalesForceAutomation/Models/Artigos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SalesForceAutomation.Models
+{
+    public class Artigo
+    {
+        public string CodArtigo
+        {
+            get;
+            set;
+        }
+
+        public string DescArtigo
+        {
+            get;
+            set;
+        }
+
+    }
+}
