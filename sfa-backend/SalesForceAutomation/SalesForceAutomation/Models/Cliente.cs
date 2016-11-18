e0221e18c715ad23138e8fb5c34dff561885bf89
FilipaRamos <fimamora@gmail.com>
2016-11-18 11:49:29 +0000

    merge

---
+++ sfa-backend/SalesForceAutomation/SalesForceAutomation/Models/Cliente.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SalesForceAutomation.Models
+{
+    public class Cliente
+    {
+        public string CodCliente
+        {
+            get;
+            set;
+        }
+
+        public string Nome
+        {
+            get;
+            set;
+        }
+
+        public string NomeFiscal
+        {
+            get;
+            set;
+        }
+
+        public string Telefone
+        {
+            get;
+            set;
+        }
+
+        public string NumContribuinte
+        {
+            get;
+            set;
+        }
+
+        public string Morada
+        {
+            get;
+            set;
+        }
+
+    }
+}
